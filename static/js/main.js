300e62665f6b1b83d57ae66b902069d5c291447b
kelvinride <jptheron99@gmail.com>
2022-02-27 00:41:46 +0200

    login restyling and fixed problems with style.css

--- static/js/main.js
+++ static/js/main.js
@@ -1,132 +1,119 @@
-;(function () {
-	
+; (function () {
+
 	'use strict';
 
 	// iPad and iPod detection	
-	var isiPad = function(){
+	var isiPad = function () {
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+	var isiPhone = function () {
+		return (
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
-	    );
+		);
 	};
 
 	// Burger Menu
-	var burgerMenu = function() {
-		$('body').on('click', '.js-fh5co-nav-toggle', function(){
-			if ( $('#fh5co-navbar').is(':visible') ) {
-				$(this).removeClass('active');	
+	var burgerMenu = function () {
+		$('body').on('click', '.js-fh5co-nav-toggle', function () {
+			if ($('#fh5co-navbar').is(':visible')) {
+				$(this).removeClass('active');
 			} else {
-				$(this).addClass('active');	
+				$(this).addClass('active');
 			}
-			
+
 		});
 	};
 
-
 	// Animate Projects
-	
-	var animateBox = function() {
-		$( "#animate-box p" ).addClass('animate-box')
-		if ( $('.animate-box').length > 0 ) {
-			$('.animate-box').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+	var animateBox = function () {
+		$("#animate-box p").addClass('animate-box')
+		if ($('.animate-box').length > 0) {
+			$('.animate-box').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
 
 					$(this.element).addClass('fadeIn animated');
-						
+
 				}
-			} , { offset: '80%' } );
+			}, { offset: '80%' });
 		}
 	};
-
 
 	// Animate Leadership
-	var animateTeam = function() {
-		if ( $('#fh5co-team').length > 0 ) {	
-			$('#fh5co-team .to-animate').each(function( k ) {
-				
+	var animateTeam = function () {
+		if ($('#fh5co-team').length > 0) {
+			$('#fh5co-team .to-animate').each(function (k) {
+
 				var el = $(this);
-				
-				setTimeout ( function () {
+
+				setTimeout(function () {
 					console.log('yaya');
 					el.addClass('fadeInUp animated');
-				},  k * 200, 'easeInOutExpo' );
-				
+				}, k * 200, 'easeInOutExpo');
+
 			});
 		}
 	};
-	var teamWayPoint = function() {
-		if ( $('#fh5co-team').length > 0 ) {
-			$('#fh5co-team').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+	var teamWayPoint = function () {
+		if ($('#fh5co-team').length > 0) {
+			$('#fh5co-team').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
 
 					setTimeout(animateTeam, 200);
-					
-					
+
+
 					$(this.element).addClass('animated');
-						
+
 				}
-			} , { offset: '80%' } );
+			}, { offset: '80%' });
 		}
 	};
 
-
 	// Animate Feature
-	var animateFeatureIcons = function() {
-		if ( $('#fh5co-services').length > 0 ) {	
-			$('#fh5co-services .to-animate').each(function( k ) {
-				
+	var animateFeatureIcons = function () {
+		if ($('#fh5co-services').length > 0) {
+			$('#fh5co-services .to-animate').each(function (k) {
+
 				var el = $(this);
-				
-				setTimeout ( function () {
+
+				setTimeout(function () {
 					el.addClass('bounceIn animated');
-				},  k * 200, 'easeInOutExpo' );
-				
+				}, k * 200, 'easeInOutExpo');
+
 			});
 		}
 	};
-	var featureIconsWayPoint = function() {
-		if ( $('#fh5co-services').length > 0 ) {
-			$('#fh5co-services').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					
-					
-					
+
+	var featureIconsWayPoint = function () {
+		if ($('#fh5co-services').length > 0) {
+			$('#fh5co-services').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
+
+
 
 					setTimeout(animateFeatureIcons, 200);
-					
-					
+
+
 					$(this.element).addClass('animated');
-						
+
 				}
-			} , { offset: '80%' } );
+			}, { offset: '80%' });
 		}
 	};
 
-
-
-	
-	
-
-	
-
-	
-	$(function(){
-		
+	$(function () {
 		burgerMenu();
 		animateBox();
 		teamWayPoint();
 		featureIconsWayPoint();
-		
-		
 
 	});
-
 
 }());
